c2ad398758064304442d604a97885d1cb4f01ee6
TicTakTok1 <66017543+TickerDev@users.noreply.github.com>
2025-07-08 19:07:26 -0500

    stop resource if no api key was added

--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ function sleep(ms) {
     const apiKey = GetConvar('fivem_israel_api_key', '')
     if (!apiKey) {
         console.error("[FIVEM-ISRAEL] Invalid API Key.\nPlease add the following to your server.cfg:\nset fivem_israel_api_key \"YOUR_API_KEY\"")
+        return StopResource(GetCurrentResourceName());
     }
     const client = new FivemIsrael({ apiKey });
 
